aa87c7f1aefc731bd77cc411ea2067b94ed4f7fd
Alex Wolf <alexrudiwolf@gmail.com>
2024-12-16 20:06:31 -0500

    comments

--- RagApp/Program.cs
+++ RagApp/Program.cs
@@ -6,10 +6,8 @@ using System.Net.Http.Json;
 using System.Text;
 using System.Text.Json;
 
-// Create Ollama and Qdrant clients
+#region createClients
 var qClient = new QdrantClient("localhost");
-
-Console.WriteLine("Ask a question. This bot is grounded in Zelda data due to RAG, so it's good at those topics.");
 
 IEmbeddingGenerator<string, Embedding<float>> generator =
     new OllamaEmbeddingGenerator(new Uri("http://localhost:11434/"), "nomic-embed-text");
@@ -17,33 +15,42 @@ IEmbeddingGenerator<string, Embedding<float>> generator =
 IChatClient chatClient =
     new OllamaChatClient(new Uri("http://localhost:11434/"), "phi3:mini");
 
+Console.WriteLine("Ask a question. This bot is grounded in Zelda data due to RAG, so it's good at those topics.");
+#endregion
+
 while (true)
 {
-    // Start the conversation with context for the AI model
+    Console.WriteLine();
+
+    // Create chat history
     List<ChatMessage> chatHistory = new();
 
-    // Get user prompt and add to chat history
-    Console.WriteLine();
+    // Get user prompt
     var userPrompt = Console.ReadLine();
 
+    // Create an embedding version of the prompt
     var promptEmbedding = (await generator.GenerateAsync(new List<string>() { userPrompt }))[0].Vector.ToArray();
 
+    // Run a vector search using the prompt embedding
     var returnedLocations = await qClient.QueryAsync(
         collectionName: "zelda-database",
         query: promptEmbedding,
         limit: 25
     );
-
-    var builder = new StringBuilder();
-    foreach (var location in returnedLocations)
-    {
-        builder.AppendLine($"{location.Payload["name"].StringValue}: {location.Payload["description"].StringValue}.");
-    }
     
     // Use this for generic chat
     //chatHistory.Add(new ChatMessage(ChatRole.User, userPrompt));
 
     // Use this for grounded chat
+    // Add the returned records from the vector search to the prompt
+    var builder = new StringBuilder();
+    foreach (var location in returnedLocations)
+    {
+        builder.AppendLine($"{location.Payload["name"].StringValue}: {location.Payload["description"].StringValue}.");
+    }
+
+    // Assemble the full prompt to the chat AI model using instructions,
+    // the original user prompt, and the retrieved relevant data
     chatHistory.Add(new ChatMessage(ChatRole.User,
                 @$"Your are an intelligent, cheerful assistant who prioritizes answers to user questions using the data in this conversation. If you do not know the answer, say 'I don't know.'. 
                 Answer the following question: 
--- populateDb/Program.cs
+++ populateDb/Program.cs
@@ -4,66 +4,71 @@ using Qdrant.Client.Grpc;
 using Shared;
 using System.Text.Json;
 
-// Create Ollama and Qdrant clients
+# region createClients
+// Create Qdrant client
 var qClient = new QdrantClient("localhost");
 
+// Create Ollama embedding client
 IEmbeddingGenerator<string, Embedding<float>> generator =
     new OllamaEmbeddingGenerator(new Uri("http://localhost:11434/"), "nomic-embed-text");
 
+// Create Ollama chat client
 IChatClient chatClient =
     new OllamaChatClient(new Uri("http://localhost:11434/"), "phi3:medium");
+#endregion
 
-try
+#region loadDataFromFiles
+
+Console.WriteLine($"Loading records...");
+var zeldaRecords = new List<ZeldaRecord>();
+
+// Add locations
+string locationsRaw = File.ReadAllText("zelda-locations.json");
+zeldaRecords.AddRange(JsonSerializer.Deserialize<List<ZeldaRecord>>(locationsRaw));
+
+// Add bosses
+string bossesRaw = File.ReadAllText("zelda-bosses.json");
+zeldaRecords.AddRange(JsonSerializer.Deserialize<List<ZeldaRecord>>(bossesRaw));
+
+// Add characters
+string charactersRaw = File.ReadAllText("zelda-characters.json");
+zeldaRecords.AddRange(JsonSerializer.Deserialize<List<ZeldaRecord>>(charactersRaw));
+
+// Add dungeons
+string dungeonsRaw = File.ReadAllText("zelda-dungeons.json");
+zeldaRecords.AddRange(JsonSerializer.Deserialize<List<ZeldaRecord>>(dungeonsRaw));
+
+// Add games
+string gamesRaw = File.ReadAllText("zelda-games.json");
+zeldaRecords.AddRange(JsonSerializer.Deserialize<List<ZeldaRecord>>(gamesRaw));
+#endregion
+
+#region vectorizeLoadedData
+
+// Create qdrant collection
+var qdrantRecords = new List<PointStruct>();
+
+foreach (var item in zeldaRecords)
 {
-    await qClient.CreateCollectionAsync("zelda-database", new VectorParams { Size = 768, Distance = Distance.Cosine });
+    item.Embedding = (await generator.GenerateAsync(
+        new List<string>() { item.Name + ": " + item.Description }))[0].Vector.ToArray();
 
-    var zeldaRecords = new List<ZeldaRecord>();
-
-    // Add locations
-    string locationsRaw = File.ReadAllText("zelda-locations.json");
-    zeldaRecords.AddRange(JsonSerializer.Deserialize<List<ZeldaRecord>>(locationsRaw));
-
-    // Add bosses
-    string bossesRaw = File.ReadAllText("zelda-bosses.json");
-    zeldaRecords.AddRange(JsonSerializer.Deserialize<List<ZeldaRecord>>(bossesRaw));
-
-    // Add characters
-    string charactersRaw = File.ReadAllText("zelda-characters.json");
-    zeldaRecords.AddRange(JsonSerializer.Deserialize<List<ZeldaRecord>>(charactersRaw));
-
-    // Add dungeons
-    string dungeonsRaw = File.ReadAllText("zelda-dungeons.json");
-    zeldaRecords.AddRange(JsonSerializer.Deserialize<List<ZeldaRecord>>(dungeonsRaw));
-
-    // Add games
-    string gamesRaw = File.ReadAllText("zelda-games.json");
-    zeldaRecords.AddRange(JsonSerializer.Deserialize<List<ZeldaRecord>>(gamesRaw));
-
-    Console.WriteLine($"Adding {zeldaRecords.Count} records...");
-
-    var qdrantRecords = new List<PointStruct>();
-
-    foreach (var item in zeldaRecords)
+    qdrantRecords.Add(new PointStruct()
     {
-        item.Embedding = (await generator.GenerateAsync(new List<string>() { item.Name + ": " + item.Description }))[0].Vector.ToArray();
-
-        qdrantRecords.Add(new PointStruct()
+        Id = new PointId((uint)new Random().Next(0, 10000000)),
+        Vectors = item.Embedding,
+        Payload =
         {
-            Id = new PointId((uint)new Random().Next(0, 10000000)),
-            Vectors = item.Embedding,
-            Payload =
-            {
-                ["name"] = item.Name,
-                ["description"] = item.Description
-            }
-        });
-    }
+            ["name"] = item.Name,
+            ["description"] = item.Description
+        }
+    });
+}
+#endregion
 
+#region insertDataIntoQdrantDB
+    await qClient.CreateCollectionAsync("zelda-database", new VectorParams { Size = 768, Distance = Distance.Cosine });
     await qClient.UpsertAsync("zelda-database", qdrantRecords);
-
     Console.WriteLine("Finished inserting records!");
-}
-catch (Exception e)
-{
-    Console.WriteLine(e.InnerException);
-}
+
+    #endregion
